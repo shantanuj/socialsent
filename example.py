2b3bdd49401fd1b9dbf577b9a4379a5f10be73c6
Vivek Kulkarni <viveksck@gmail.com>
2017-09-17 11:45:17 -0700

    Some modifications.

--- example.py
+++ example.py
@@ -9,16 +9,19 @@ if __name__ == "__main__":
     print "Evaluting only binary classification performance on General Inquirer lexicon"
     lexicon = lexicons.load_lexicon("inquirer", remove_neutral=True)
     pos_seeds, neg_seeds = seeds.hist_seeds()
+    seeds_map = {1:pos_seeds, -1:neg_seeds}
     embeddings = create_representation("GIGA", "data/example_embeddings/glove.6B.100d.txt",
         set(lexicon.keys()).union(pos_seeds).union(neg_seeds))
     eval_words = [word for word in embeddings.iw
             if not word in pos_seeds 
             and not word in neg_seeds]
     # Using SentProp with 10 neighbors and beta=0.99
-    polarities = random_walk(embeddings, pos_seeds, neg_seeds, beta=0.99, nn=10,
+    polarities = random_walk(embeddings, seeds_map, beta=0.99, nn=10,
             sym=True, arccos=True)
 
-    acc, auc, avg_per  = binary_metrics(polarities, lexicon, eval_words)
+
+    point_estimates = dict([(w,polarities[w].most_common()[0][0]) for w in polarities])
+    acc, auc, avg_per  = binary_metrics(point_estimates, lexicon, eval_words)
     print "Accuracy with best threshold: {:0.2f}".format(acc)
     print "ROC AUC: {:0.2f}".format(auc)
     print "Average precision score: {:0.2f}".format(avg_per)
